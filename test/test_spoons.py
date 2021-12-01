73df88adc85eb05224a836f0e6562782bd4515e4
Derek Todd <derekhtodd@yahoo.co.uk>
2021-12-01 15:20:19 +0000

    still setting up testing

---
+++ test/test_spoons.py
@@ -0,0 +1,9 @@
+import unittest
+
+def  test_initialiseDeck(self):
+    result = self.spoons.initialiseDeck()
+    self.assertEqual(52, len(result))
+
+
+if __name__ == "__main__":
+    unittest.main()
