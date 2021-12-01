aec6071f724608a1355c0bc9be9beca693aa611e
Derek Todd <derekhtodd@yahoo.co.uk>
2021-12-01 16:35:38 +0000

    Another test

--- test/test_spoons.py
+++ test/test_spoons.py
@@ -2,23 +2,31 @@ import unittest
 import sys
 sys.path.append('/home/derek/Documents/UoG/GA1/FPSE/Week3/unitTesing/PairProgramming/PythonOOCardGames')
 from src.Spoons import Spoons
-# from src.PlayingCard import PlayingCard
+from src.PlayingCard import PlayingCard
 
 
 
 class SpoonsTest(unittest.TestCase):
     spoons = Spoons()
+    playing_card = PlayingCard()
     def test_test(self):
         self.spoons.assertEqual(True, True)
 
 
-    def  test_initialiseDeck(self):
+    def  test_initialiseDeck_size(self):
         result = self.spoons.initialiseDeck()
         self.assertEqual(52, len(result))
 
+    def test_initialiseDeck_shuffled(self):
+        unshuffled_deck = self.playing_card.generate_deck()
+        # initDeckOutput = self.playing_card.generate_deck()
+        initDeckOutput = self.spoons.initialiseDeck()
+        self.assertNotEqual(unshuffled_deck, initDeckOutput)
+
+
+
 def main():
     unittest.main()
-
 
 
 if __name__ == "__main__":
